7232761e7fdba92d56df5e5a233db089e0e3bc70
balrajchavan <chavab01@mc-s102299.lan>
2020-06-27 10:38:11 +0100

    First cimmit

---
+++ SeleniumFramework/src/test/java/POM_Tests/ExtentReportsWithTestNG.java
@@ -0,0 +1,52 @@
+package POM_Tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import POM_Pages.AppleSearchPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportsWithTestNG {
+	
+	private static WebDriver driver;
+
+	@BeforeMethod
+	@BeforeTest
+	public void setUp() {
+		
+		
+		
+		WebDriverManager.chromedriver().setup(); 
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		
+	}
+
+	
+	@Test
+	public void test1() {
+		
+		AppleSearchPage searchPageObj = new AppleSearchPage(driver);
+		
+		driver.get("https://apple.com");
+		searchPageObj.enterSearchBox();
+		searchPageObj.inputSearch("macbook");
+		searchPageObj.searchEnter();
+		
+	}
+	
+	@AfterTest
+	public void tearDownTest() {
+		
+		driver.close();
+		driver.quit();
+		System.out.println("Test completed successfully");
+
+	}
+}
